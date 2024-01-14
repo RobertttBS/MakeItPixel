c7989c70912ba37cb3f5b4a8c39625a30de5fef5
謝柏陞 <k0983357798@gmail.com>
2024-01-14 15:53:17 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 
-# g++ -lpthread -ffast-math -msse -lm src/* -Iinclude -lsfml-graphics -o makeitpixel
-# g++ -O3 src/* -Iinclude -lsfml-graphics -o makeitpixel
-g++ -ffast-math -msse src/* -Iinclude -lsfml-graphics -o makeitpixel
-# time ./makeitpixel -c examples/config1.json -o doc /home/robert/Desktop/MakeItPixel/examples/monalisa.png
-
-
-# time ./makeitpixel -c examples/config3.json -o doc IMG_7210.jpeg
+g++ -lpthread src/* -Iinclude -lsfml-graphics -o makeitpixel
 
 time ./makeitpixel -c examples/config3.json -o doc test.jpg
-
 
 # config1.json, config3.json 對 dither 的優化比較明顯
